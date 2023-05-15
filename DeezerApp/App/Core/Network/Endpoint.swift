397e45b7d07124c7c915d829433915721cb1010f
onurduyar <onurdyr510@gmail.com>
2023-05-15 14:09:55 +0300

    network-layer was added.

---
+++ DeezerApp/App/Core/Network/Endpoint.swift
@@ -0,0 +1,18 @@
+//
+//  Endpoint.swift
+//  DeezerCase
+//
+//  Created by Onur Duyar on 8.05.2023.
+//
+
+import Foundation
+
+enum Endpoint: String {
+    case genre = "/genre/"
+    case artists = "/artists"
+    case album = "/album/"
+    case artist = "/artist/"
+    case albums = "/albums"
+    case tracks = "/tracks"
+}
+
